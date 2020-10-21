9c1487ef1236703d1761b2ab4dd5f5bbb32b8c2f
June Young Kang <jkang34@gmail.com>
2020-10-21 20:01:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,13 @@
 # Write your code here!
+game_hash = {
+  :home => {
+    :team_name => "name",
+    :colors => [],
+    :players
+  },
+  :away => {
+    :team_name,
+    :colors,
+    :players
+  }
+}
